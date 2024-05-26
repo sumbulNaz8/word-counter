e35735533d0cc30bf8194dbf1bbdb42156a92a50
sunbulnaz <161361478+sunbulnaz@users.noreply.github.com>
2024-05-26 14:17:23 +0500

    update version and run npx command

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.italic `\n\t-----------------
+                                        WELCOME TO MY WORD COUNTER   
+                                                                        -----------------------------------\n`);
+do {
+    const answers = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: chalk.blueBright("Enter your sentence to count the word:")
+        }
+    ]);
+    const words = answers.sentence.trim().split(" ");
+    console.log(words);
+    console.log(chalk.magentaBright(`Your sentence word count is ${words.length}`));
+    let ans = await inquirer.prompt([
+        {
+            name: 'continue',
+            type: 'confirm',
+            message: (chalk.bold.whiteBright.bold('Do you want to again continue?')),
+            default: true
+        }
+    ]);
+    if (ans.continue === false) {
+        console.log(chalk.green('you excited from "WORD COUNTER"...\n Goodbye have a nice day ...\n\t'));
+        break;
+    }
+} while (true);
