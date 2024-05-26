109e608c7122950968072e1c0c8390f508162ee0
sunbulnaz <161361478+sunbulnaz@users.noreply.github.com>
2024-05-26 21:52:40 +0500

    push my 'WORD COUNTER' projects

--- index.ts
+++ index.ts
@@ -37,12 +37,12 @@ if (ans.continue === false ){
   }
   }while(true)
   
-/*git init
+
+/*echo "# word-counter" >> README.md
+git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/sumbulNaz8/word-counter.git
-git push -u origin mai*/
-
-
+git push -u origin main*/
 
